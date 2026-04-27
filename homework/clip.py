629babd6e07f58d521a674a6412e89c1be7e6b1c
timdeng2 <timdeng306@gmail.com>
2026-04-27 02:43:02 -0700

    params

--- homework/clip.py
+++ homework/clip.py
@@ -255,11 +255,11 @@ def get_target_modules_for_lora(model: nn.Module) -> list[str]:
 def train(
     data_dir: Path | None = None,
     output_dir: str = "clip_model",
-    num_train_epochs: float = 0.05,  # for debugging purpose, increase this once the dry run works
-    per_device_train_batch_size: int = 1024,
-    gradient_accumulation_steps: int = 1,
+    num_train_epochs: float = 0.25,  # for debugging purpose, increase this once the dry run works
+    per_device_train_batch_size: int = 32,
+    gradient_accumulation_steps: int = 8,
     learning_rate: float = 5e-4,
-    num_workers: int = 16,
+    num_workers: int = 2,
 ):
     vlm = BaseVLM()
 
